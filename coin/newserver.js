60aab78c0f1659d8cf335a141e7c299d8ea3cf06
karthik006 <karthikknidhi@csu.fullerton.edu>
2016-04-10 11:54:54 -0700

    newserver.js

--- coin/newserver.js
+++ coin/newserver.js
@@ -1,5 +1,5 @@
 var express = require("express"),
-redis = require("redis")
+    redis = require("redis"),
     http = require("http"),
     app = express(),
     bodyParser = require('body-parser');
@@ -19,56 +19,58 @@ redisClient.mget(["wins", "losses"], function (err, results) {
         // exit the function
         return;
     }
-wins = parseInt(results[0], 10) || 0;
-losses = parseInt(results[1], 10) || 0;
+    wins = parseInt(results[0], 10) || 0;
+    losses = parseInt(results[1], 10) || 0;
 
-app.post("/flip", function (req, res) {
+    app.post("/flip", function (req, res) {
 
-    var selection = req.body.call;
-    console.log(selection);
-    console.log("I choose " + selection);
-
-
-    var result = Math.floor(Math.random() * 2) === 0 ? 'heads' : 'tails'; 
-    console.log("Randomly generated: " + result);
+        var selection = req.body.call;
+        console.log(selection);
+        console.log("I choose " + selection);
 
 
-    if (result === selection) {
-        wins = wins + 1;
-		redisClient.incr("wins");
-		console.log("wins:" + wins);
-        console.log("losses:" + losses);
-		res.send(JSON.stringify({
-        "result": "win"
-    }));
+        var result = Math.floor(Math.random() * 2) === 0 ? 'heads' : 'tails';
+        console.log("Randomly generated: " + result);
 
-    } else {
-        losses = losses + 1;
-		redisClient.incr("losses");
-        console.log("wins:" + wins);
-        console.log("losses:" + losses);
-     res.send(JSON.stringify({
-        "result": "lost"
-    }));
-    }
-});
-});
+
+        if (result === selection) {
+            wins = wins + 1;
+            redisClient.incr("wins");
+            console.log("wins:" + wins);
+            console.log("losses:" + losses);
+            res.send(JSON.stringify({
+                "result": "win"
+            }));
+
+        } else {
+            losses = losses + 1;
+            redisClient.incr("losses");
+            console.log("wins:" + wins);
+            console.log("losses:" + losses);
+            res.send(JSON.stringify({
+                "result": "lost"
+            }));
+        }
+    });
+
 app.get("/stats", function (req, res) {
-    res.send(JSON.stringify({
+    
+		res.send(JSON.stringify({
         "wins": wins,
         "losses": losses
     }));
-	
-app.delete("/stats",function(req,res){
-	redisClient.set("wins", 0);
-	redisClient.set("losses", 0);
-	wins = 0;
-	losses = 0;
-	res.send(JSON.stringify({
-        "wins": 0,
-        "losses": 0
-    }));
-	
+
+    app.delete("/stats", function (req, res) {
+        redisClient.set("wins", "0");
+        redisClient.set("losses", "0");
+		wins=0;
+		losses=0;
+        res.send(JSON.stringify({
+            "wins": wins,
+            "losses": losses
+        }));
+
+    });
 });
 });
 app.listen(3000, function () {

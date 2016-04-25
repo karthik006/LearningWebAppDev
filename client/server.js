02bf96d5a4419fc8f00034f64a8f7b1ded7f612b
karthik006 <karthikknidhi@csu.fullerton.edu>
2016-04-25 15:56:33 -0700

    Assignment 8

--- client/server.js
+++ client/server.js
@@ -24,50 +24,53 @@ mongoose.connect("mongodb://localhost/assignment8");
 // This is our mongoose model for todos
 var ToDoSchema = mongoose.Schema({
     description: String,
-    tags: [ String ]
+    tags: [String]
 });
 
 var ToDo = mongoose.model("ToDo", ToDoSchema);
 
 app.get("/todos.json", function (req, res) {
     ToDo.find({}, function (err, toDos) {
-	res.json(toDos);
+        res.json(toDos);
     });
 });
 
-app.get("/", function(req, res){
-  res.sendFile("/index.html");
+app.get("/", function (req, res) {
+    res.sendFile("/index.html");
 });
 
-io.on("connection", function(socket){
-  socket.on("todoItem", function(data){
-	
-    io.emit("todoItem", data);
-  });
+io.on("connection", function (socket) {
+    socket.on("todoItem", function (data) {
+
+        io.emit("todoItem", data);
+    });
 });
 
 app.post("/todos", function (req, res) {
     console.log(req.body);
-    var newToDo = new ToDo({"description":req.body.description, "tags":req.body.tags});
+    var newToDo = new ToDo({
+        "description": req.body.description,
+        "tags": req.body.tags
+    });
     newToDo.save(function (err, result) {
-	if (err !== null) {
-	    // the element did not get saved!
-	    console.log(err);
-	    res.send("ERROR");
-	} else {
-	    // our client expects *all* of the todo items to be returned, so we'll do
-	    // an additional request to maintain compatibility
-	    ToDo.find({}, function (err, result) {
-		if (err !== null) {
-		    // the element did not get saved!
-		    res.send("ERROR");
-		}
-		res.json(result);
-	    });
-	}
+        if (err !== null) {
+            // the element did not get saved!
+            console.log(err);
+            res.send("ERROR");
+        } else {
+            // our client expects *all* of the todo items to be returned, so we'll do
+            // an additional request to maintain compatibility
+            ToDo.find({}, function (err, result) {
+                if (err !== null) {
+                    // the element did not get saved!
+                    res.send("ERROR");
+                }
+                res.json(result);
+            });
+        }
     });
 });
 
-http.listen(3000, function(){
-  console.log("listening on *:3000");
+http.listen(3000, function () {
+    console.log("listening on *:3000");
 });

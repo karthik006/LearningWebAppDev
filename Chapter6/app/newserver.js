d360c40555151b3032eb4ffeebaf75be55d684cb
karthik006 <karthikknidhi@csu.fullerton.edu>
2016-05-14 15:34:12 -0700

    removed the header problem (<<<<<<<<<<header statement on the first line) and committed 5 files

--- Chapter6/app/newserver.js
+++ Chapter6/app/newserver.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 var express=require("express"),
-    http=require("http"),app=express();
+http=require("http"),
+app=express();
 
 app.locals.wins=0;
 app.locals.losses=0;
@@ -40,55 +40,7 @@ app.get("/stats",function(req,res){
 
 });
 
-http.createServer(app).listen(3000);
-=======
-var express = require("express"),
-    http = require("http"),
-    app = express(),
-    bodyParser = require('body-parser');
-wins = 0;
-losses = 0;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.post("/flip", function (req, res) {
-
-    var selection = req.body.call;
-    console.log(selection);
-    console.log("I choose " + selection);
-
-
-    var result = Math.floor(Math.random() * 2) === 0 ? 'heads' : 'tails'; 
-    console.log("Randomly generated: " + result);
-
-
-    if (result === selection) {
-        wins = wins + 1;
-		console.log("wins:" + wins);
-        console.log("losses:" + losses);
-		res.send(JSON.stringify({
-        "result": "win"
-    }));
-
-    } else {
-        losses = losses + 1;
-        console.log("wins:" + wins);
-        console.log("losses:" + losses);
-     res.send(JSON.stringify({
-        "result": "lost"
-    }));
-    }
-
-});
-
-app.get("/stats", function (req, res) {
-    res.send(JSON.stringify({
-        "wins": wins,
-        "losses": losses
-    }));
-});
 app.listen(3000, function () {
     console.log('listening on port 3000');
 });
->>>>>>> b1a8cc41274a7a73ee39d05e7231cc08845fdbe8
+
